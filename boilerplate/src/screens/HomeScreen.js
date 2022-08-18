cdf466b6594ff5a2018bd8b32d2d73b29ea727c4
Guilherme Morone <guimorone@gmail.com>
2022-08-18 07:34:36 -0300

    diagrams and boilerplate added

---
+++ boilerplate/src/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+
+// TouchableOpacity == Div que detecta toques (tem onPress)
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go to Components Demo"
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate('List')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
